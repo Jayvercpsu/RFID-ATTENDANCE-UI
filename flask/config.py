94ec247583dd66666d5d5c0176620aed69e46b4a
Jayver Algadipe <jayvercpsu@gmail.com>
2025-08-03 11:24:09 +0800

    add static files and include sidebar shortcut

--- flask/config.py
+++ flask/config.py
@@ -1,3 +1,5 @@
 MAIN_PAGE = "pages/index.html"
 ADMIN_LOGIN_PAGE = "pages/admin/admin-login.html"
 ADMIN_DASHBOARD_PAGE = "pages/admin/dashboard.html"
+ADMIN_STUDENTS_PAGE = "pages/admin/students.html"
+ADMIN_ATTENDANCE_LOGS_PAGE = "pages/admin/attendance-logs.html"
--- flask/pages.py
+++ flask/pages.py
@@ -1,13 +1,13 @@
 # Create a Blueprint for pages
 from flask import Blueprint, render_template
 
-from config import ADMIN_DASHBOARD_PAGE, ADMIN_LOGIN_PAGE, MAIN_PAGE
+from config import ADMIN_DASHBOARD_PAGE, ADMIN_LOGIN_PAGE, ADMIN_STUDENTS_PAGE, MAIN_PAGE
 
 
 pages_bp = Blueprint("pages", __name__)
 
 @pages_bp.route("/")
-def home():
+def home(): 
     return render_template(MAIN_PAGE)
 
 @pages_bp.route("/admin/login")
@@ -17,3 +17,11 @@ def admin_login():
 @pages_bp.route("/admin/dashboard")
 def admin_dashboard():
     return render_template(ADMIN_DASHBOARD_PAGE)
+
+@pages_bp.route("/admin/students")
+def admin_students():
+    return render_template(ADMIN_STUDENTS_PAGE)
+
+@pages_bp.route("/admin/attendance-logs")
+def admin_attendance_logs():
+    return render_template(ADMIN_ATTENDANCE_LOGS_PAGE)
